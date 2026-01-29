80a0d02018e1256b6d6ca3bafcbbb34f144f15f1
DylanPerera123 <DYLAN.PERERA12@student.qcc.cuny.edu>
2026-01-29 01:31:57 +0000

    lab 1

---
+++ Lab1/Operators.java
@@ -0,0 +1,20 @@
+public class Operators {
+    public static void main(String[] args ){
+        int x = 1;
+        double y = 2.0;
+        System.out.print(x+y);
+        System.out.println(3.0/2);
+        //int divison
+        System.out.println( 5/2);// whole number
+        System.out.println(5 % 2);// remainder
+
+        x = 3;
+        System.out.println("Post increament");
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println("Pre increament");
+        System.out.println(++x);
+        System.out.println(x);
+    }
+} 
+        
---
+++ Lab1/ScannerInput.java
@@ -0,0 +1,16 @@
+import java.util.Sacnner;
+
+public class ScannerInput {
+    public static void main (String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int x = inpit.nextINT();
+        System.out.println(x);
+        input.close(); 
+        Systeem.out.print(x);
+        System.out.print(y);
+        System.out.print(s:"\n")
+        System.out.println(z);
+        input.close();
+    
+} 
