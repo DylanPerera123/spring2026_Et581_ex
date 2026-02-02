29ac0d55813419aa289fdddfff33e98e2378cde6
DylanPerera123 <DYLAN.PERERA12@student.qcc.cuny.edu>
2026-02-02 02:33:15 +0000

    HW1Q1

---
+++ Homework 1/HW1Q1.java
@@ -0,0 +1,11 @@
+public class HW1Q1 {
+    public static void main(String[] args) {
+        int age = 26;
+        double height = 5.11;
+        char grade = 'A';
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+    }
+}
